7b219504087d5fbcec0ab0672bd76f4281fa6415
Ryan Lee <drdgvhbh@gmail.com>
2019-05-14 23:32:18 -0400

    Rename method parameters to be consistent

--- internal/middlewares.go
+++ internal/middlewares.go
@@ -5,8 +5,8 @@ import (
 )
 
 func ContentTypeMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Content-Type", "application/json")
-		next.ServeHTTP(w, r)
+	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		writer.Header().Add("Content-Type", "application/json")
+		next.ServeHTTP(writer, request)
 	})
 }
