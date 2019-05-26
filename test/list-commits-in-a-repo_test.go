f69e886a6a1619c11890f0f7abb2cd9a905c0ade
Ryan Lee <drdgvhbh@gmail.com>
2019-05-25 22:10:56 -0400

    Add Author data to GET/ Commits

--- test/list-commits-in-a-repo_test.go
+++ test/list-commits-in-a-repo_test.go
@@ -86,27 +86,57 @@ func (suite *ListCommitsInARepoTestSuite) TestDerp() {
 		`[
 			{
 				"hash": "625d85387d80a56a26a5c7ff28d84e49afef2635",
-				"summary": "This is me adding text to a file"
+				"summary": "This is me adding text to a file",
+				"author": {
+					"name": "Ryan Lee",
+					"email": "drdgvhbh@gmail.com",
+					"timestamp": "2019-05-25T17:19:52-04:00"
+				}
 			},
 			{
 				"hash": "22b86bcafbc105b45d62772546ef1d2b4bef74f1",
-				"summary": "this is me renaming a directory"
+				"summary": "this is me renaming a directory",
+				"author": {
+					"name": "Ryan Lee",
+					"email": "drdgvhbh@gmail.com",
+					"timestamp": "2019-05-25T15:18:47-04:00"
+				}
 			},
 			{
 				"hash": "a004980430dffe788c44d97584a2818c54bf6e54",
-				"summary": "this is me adding a file in a subdirectory"
+				"summary": "this is me adding a file in a subdirectory",
+				"author": {
+					"name": "Ryan Lee",
+					"email": "drdgvhbh@gmail.com",
+					"timestamp": "2019-05-25T15:18:15-04:00"
+				}
 			},
 			{
 				"hash": "b322ecd55ffee1cdfd42a68f49f201129d9ef023",
-				"summary": "this is me deleting a file"
+				"summary": "this is me deleting a file",
+				"author": {
+					"name": "Ryan Lee",
+					"email": "drdgvhbh@gmail.com",
+					"timestamp": "2019-05-25T15:17:35-04:00"
+				}
 			},
 			{
 				"hash": "9649993898da5b19e530a2750ac5fb6fea2d56f8",
-				"summary": "This is me adding three new files"
+				"summary": "This is me adding three new files",
+				"author": {
+					"name": "Ryan Lee",
+					"email": "drdgvhbh@gmail.com",
+					"timestamp": "2019-05-25T15:16:38-04:00"
+				}
 			},
 			{
 				"hash": "a82386953aa55fe38dd79a3b65742e39298afc2f",
-				"summary": "This is the start of the repo"
+				"summary": "This is the start of the repo",
+				"author": {
+					"name": "Ryan Lee",
+					"email": "drdgvhbh@gmail.com",
+					"timestamp": "2019-05-25T15:15:37-04:00"
+				}
 			}
 		]`,
 		string(data),
