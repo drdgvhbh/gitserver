132b7998710cebbff93f67a850e9088497ddd6b8
Ryan Lee <drdgvhbh@gmail.com>
2019-05-28 22:34:17 -0400

    Add ReferenceIter support

--- internal/git/repository.go
+++ internal/git/repository.go
@@ -7,6 +7,7 @@ import (
 	"gopkg.in/src-d/go-git.v4"
 	"gopkg.in/src-d/go-git.v4/plumbing"
 	"gopkg.in/src-d/go-git.v4/plumbing/object"
+	"gopkg.in/src-d/go-git.v4/plumbing/storer"
 )
 
 type Hash [20]byte
@@ -36,9 +37,57 @@ func (s SignatureWrapper) Timestamp() time.Time {
 type LogOptions struct {
 	From Hash
 }
+type ReferenceName string
 
 type Reference interface {
 	Hash() Hash
+	Name() ReferenceName
+}
+
+type ReferenceIter interface {
+	Next() (Reference, error)
+	ForEach(func(Reference) error) error
+	Close()
+}
+
+type GitReferenceIter struct {
+	Wrapee storer.ReferenceIter
+}
+
+func (iter *GitReferenceIter) Next() (Reference, error) {
+	reference, err := iter.Wrapee.Next()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &GitReference{
+		Wrapee: reference,
+	}, nil
+}
+
+func (iter *GitReferenceIter) ForEach(fn func(Reference) error) error {
+	return iter.Wrapee.ForEach(func(reference *plumbing.Reference) error {
+		return fn(&GitReference{
+			Wrapee: reference,
+		})
+	})
+}
+
+func (iter *GitReferenceIter) Close() {
+	iter.Wrapee.Close()
+}
+
+type GitReference struct {
+	Wrapee *plumbing.Reference
+}
+
+func (ref *GitReference) Hash() Hash {
+	return Hash(ref.Wrapee.Hash())
+}
+
+func (ref *GitReference) Name() ReferenceName {
+	return ReferenceName(ref.Wrapee.Name())
 }
 
 type Commit interface {
@@ -111,6 +160,7 @@ func (iter *GitCommitIter) Close() {
 type Repository interface {
 	Head() (Reference, error)
 	Log(options *LogOptions) (CommitIter, error)
+	References() (ReferenceIter, error)
 }
 
 type GitRepository struct {
@@ -131,6 +181,7 @@ func (repo *GitRepository) Log(options *LogOptions) (CommitIter, error) {
 	wrappedOpts := &git.LogOptions{
 		From: plumbing.Hash(options.From),
 	}
+
 	commitIter, err := repo.Wrapee.Log(wrappedOpts)
 	if err != nil {
 		return nil, err
@@ -139,10 +190,13 @@ func (repo *GitRepository) Log(options *LogOptions) (CommitIter, error) {
 	return &GitCommitIter{Wrapee: commitIter}, nil
 }
 
-type GitReference struct {
-	Wrapee *plumbing.Reference
-}
+func (repo *GitRepository) References() (ReferenceIter, error) {
+	wrappedIter, err := repo.Wrapee.References()
 
-func (ref *GitReference) Hash() Hash {
-	return Hash(ref.Wrapee.Hash())
+	if err != nil {
+		return nil, err
+	}
+
+	return &GitReferenceIter{Wrapee: wrappedIter}, nil
+
 }
