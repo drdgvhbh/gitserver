402023aace7ee93a15cdaf557809192d0a39433c
Ryan Lee <drdgvhbh@gmail.com>
2019-06-01 20:49:31 -0400

    Fix not being able to list the commits of a submodule
    The .git files are in to the root repository .git folder for a submodule. There is a .git file in the submodule stating the path of the actual files.

--- internal/git/reader.go
+++ internal/git/reader.go
@@ -1,11 +1,18 @@
 package git
 
 import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+
 	"gopkg.in/src-d/go-billy.v4"
 	gogit "gopkg.in/src-d/go-git.v4"
 	"gopkg.in/src-d/go-git.v4/plumbing/cache"
 	"gopkg.in/src-d/go-git.v4/storage/filesystem"
 )
+
+const dotGitFolderName = ".git"
 
 type Reader interface {
 	Open(path string) (Repository, error)
@@ -23,12 +30,14 @@ func NewReader(fileSystem billy.Filesystem) Reader {
 
 // Open opens a git repository from the given path
 func (reader *StorageReader) Open(path string) (Repository, error) {
-	repoRoot, err := reader.fileSystem.Chroot(path)
+	folder := findDotGitFolder(path)
+
+	dotgitFolder, err := reader.fileSystem.Chroot(folder)
 	if err != nil {
 		return nil, err
 	}
 
-	dotgitFolder, err := repoRoot.Chroot(".git")
+	repoRoot, err := reader.fileSystem.Chroot(path)
 	if err != nil {
 		return nil, err
 	}
@@ -43,4 +52,29 @@ func (reader *StorageReader) Open(path string) (Repository, error) {
 	}
 
 	return &GitRepository{Wrapee: repo}, nil
+}
+
+func findDotGitFolder(path string) string {
+	dotGitPath := fmt.Sprintf("%s/.git", path)
+	dotGitStat, err := os.Stat(dotGitPath)
+	if err != nil {
+		return ""
+	}
+
+	repositoryIsNotASubmodule := dotGitStat.IsDir()
+	if repositoryIsNotASubmodule {
+		return dotGitPath
+	}
+
+	dotGitData, err := ioutil.ReadFile(dotGitPath)
+	if err != nil {
+		return ""
+	}
+
+	relativeDotGitPath := strings.Trim(
+		fmt.Sprintf("%s/%s",
+			path,
+			strings.Split(string(dotGitData), " ")[1]), "\n")
+
+	return relativeDotGitPath
 }
