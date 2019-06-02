d27af71fa80fd61f1a8d4e0faa38abbd39c6fc0d
Ryan Lee <drdgvhbh@gmail.com>
2019-06-01 22:07:07 -0400

    Clean tests to be able to reuse logic for other tests

--- test/list-commits-in-a-repo_test.go
+++ test/list-commits-in-a-repo_test.go
@@ -1,91 +1,39 @@
 package test
 
 import (
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"net/http/httptest"
 	"net/url"
-	"path"
-	"runtime"
-	"strings"
 	"testing"
 
-	"github.com/drdgvhbh/gitserver/internal/response"
-
-	"gopkg.in/src-d/go-git.v4/plumbing/cache"
-	"gopkg.in/src-d/go-git.v4/storage/filesystem"
-
-	"gopkg.in/src-d/go-billy.v4/osfs"
-
-	"gopkg.in/src-d/go-git.v4"
-
-	"github.com/drdgvhbh/gitserver/internal"
 	"github.com/stretchr/testify/suite"
 )
 
 type ListCommitsInARepoTestSuite struct {
-	suite.Suite
-	repo *git.Repository
+	simpleTestSuite
 }
 
-func (suite *ListCommitsInARepoTestSuite) SetupTest() {
-
-}
-
-func (suite *ListCommitsInARepoTestSuite) TestDerp() {
-	_, filename, _, ok := runtime.Caller(0)
-	if !ok {
-		panic("No caller information")
-	}
-	repoURL, err := url.Parse(fmt.Sprintf("%s/simple-git-repo", path.Dir(filename)))
-	suite.NoError(err)
-
-	directory, err := ioutil.TempDir("", "basictest")
-	suite.NoError(err)
-	fs := osfs.New(directory)
-
-	err = fs.MkdirAll(".git", 775)
-	suite.NoError(err)
-
-	dotgit, err := fs.Chroot(".git")
-	suite.NoError(err)
-
-	storage := filesystem.NewStorageWithOptions(
-		dotgit,
-		cache.NewObjectLRUDefault(),
-		filesystem.Options{KeepDescriptors: true})
-	_, err = git.Clone(storage, fs, &git.CloneOptions{
-		URL: repoURL.String(),
-	})
-	suite.NoError(err)
-
-	rootHandler := internal.NewRootHandler(osfs.New(""))
-	testServer := httptest.NewServer(rootHandler)
+func (suite *ListCommitsInARepoTestSuite) TestListCommits() {
+	testServer := suite.testServer
+	basePath := suite.basePath
+	currentDir := suite.currentDir
 
 	defer testServer.Close()
 
-	repoPath := fmt.Sprintf("%s", strings.Replace(directory, "/", "|", -1))
 	reqURL, err := url.Parse(
-		fmt.Sprintf("%s/v1/repositories/%s/commits", testServer.URL, repoPath))
+		fmt.Sprintf("%s/v1/repositories/%s/commits", testServer.URL, basePath))
 	suite.NoError(err)
 
 	req, err := http.NewRequest("GET", reqURL.String(), nil)
 	suite.NoError(err)
-	req.Header.Add("Authorization", "e8b8dc29-d1d9-495d-b509-4dde3701018b")
 
-	resp, err := testServer.Client().Do(req)
-	suite.NoError(err)
+	data := executeRequest(testServer, req, suite)
 
-	body, err := ioutil.ReadAll(resp.Body)
-	suite.NoError(err)
-
-	var responseBody *response.Definition
-	err = json.Unmarshal(body, &responseBody)
-	suite.NoError(err)
-
-	data, err := json.Marshal(responseBody.Data)
+	testDataFilePath := fmt.Sprintf(
+		"%s/test-responses/list-commits-simple.json",
+		currentDir)
+	testData, err := ioutil.ReadFile(testDataFilePath)
 	suite.NoError(err)
 
 	suite.Assert().JSONEq(
